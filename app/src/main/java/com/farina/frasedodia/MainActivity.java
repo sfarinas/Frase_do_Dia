8e32498bd5585076cb0c182a8729ef2f4b6a9348
sfarinas <sfarinas@gmail.com>
2022-09-27 00:38:38 -0300

    add mais frases 27/09/2022

--- app/src/main/java/com/farina/frasedodia/MainActivity.java
+++ app/src/main/java/com/farina/frasedodia/MainActivity.java
@@ -18,15 +18,42 @@ public class MainActivity extends AppCompatActivity {
 
     public void gerarNovaFrase(View view){
         String[] frase = {
-            "Niguem paga almoço de graça",
+            "Niguem paga almoço de graça",//1
             "Deixa de ser precoce ...",
             "Estudar por Osmose não é bom, mais eu faço",
-            "FLAMENGOOOO!"
+            "FLAMENGOOOO!", //Cabo
+            "Amanda Vai Durmir", //Simone
+            "Sara come logo",    //Simone
+            "Parei de jogar",//salomao
+            "Quebrei meu tornozelo por que pisei errado"  //coba
         };
-        int numeros = new Random().nextInt(4);
+        int numeros = new Random().nextInt(8);
 
         TextView texto = findViewById(R.id.fraseTexto);
         texto.setText( frase[numeros] );
+
+        switch (numeros){
+            case 0: case 1: case 2: case 3 :
+                TextView texto2 = findViewById(R.id.textAutor);
+                texto2.setText("Alexandre Cabo");
+                break;
+            case 4: case 5:
+                TextView texto3 = findViewById(R.id.textAutor);
+                texto3.setText("Simone Farina");
+                break;
+            case 6:
+                TextView texto4 = findViewById(R.id.textAutor);
+                texto4.setText("Salomão Farina");
+                break;
+            case 7:
+                TextView texto5 = findViewById(R.id.textAutor);
+                texto5.setText("Rennan Santos (Koba)");
+                break;
+            default:
+                TextView texto99 = findViewById(R.id.textAutor);
+                texto99.setText("Nada");
+
+        }
 
     }
 
